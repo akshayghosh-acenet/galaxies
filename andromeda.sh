2c4cd8ba508c21dc8252f004086c1c8a087e3a7c
Akshay Ghosh <akshay.ghosh@gmail.com>
2024-03-20 15:15:31 -0300

    Added files on information of galaxies

---
+++ andromeda.sh
@@ -0,0 +1,5 @@
+Galaxy name = Andromeda
+Galaxy black hole mass = 1e5
+Galaxy black hole spin = ?
+Galaxy redshift = 0
+Galaxy brightness variabiliy = None
---
+++ re-j1034-396.sh
@@ -0,0 +1,5 @@
+Galaxy name = ___
+Galaxy black hole mass = ___
+Galaxy black hole spin = ___
+Galaxy redshift = ___
+Galaxy brightness variabiliy = ___
